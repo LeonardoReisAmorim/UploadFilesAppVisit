b77d059f1778a0251c39a17200fba104bbd1323c
Leonardo Amorim <leonardo.amorim@gomara.tech>
2024-02-21 01:24:11 -0300

    create website upload file and create place VISIT APP

---
+++ createPlace.js
@@ -0,0 +1,17 @@
+document.getElementById("arquivoId").innerHTML = localStorage['arquivoId'] || 'defaultValue';
+
+function CreatePlace(){
+    fetch("https://localhost:7011/Lugar", {
+        method: "POST",
+        body: JSON.stringify({
+            nome: "",
+            descricao: "",
+            arquivoId: 0,
+            cidadeId: 0,
+            imagem: ""
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    });
+}
---
+++ index.js
@@ -0,0 +1,29 @@
+function InputFileChange() {
+    let quantidade = document.getElementById("selecao-arquivo").files.length;
+    if (quantidade > 0) {
+        document.getElementById('salvar-anexos').style.display = "block";
+    }
+    else {
+        document.getElementById('salvar-anexos').style.display = "none";
+    }
+    document.querySelector("body > div.custom-file > label").innerHTML = `${quantidade} Arquivo(s) Selecionado(s)`; 
+}
+
+function ChamarAjaxComArquivos() {
+    var formData = new FormData();
+    var totalFiles = document.getElementById("selecao-arquivo").files.length;
+
+    for (var i = 0; i < totalFiles; i++) {
+        var file = document.getElementById("selecao-arquivo").files[i];
+
+        formData.append("selecao-arquivo", file);
+    }
+
+    fetch('https://localhost:7011/Arquivo', {
+        method: "POST",
+        body: formData
+    }).then(response => response.json()).then(arquivo => {
+        localStorage['arquivoId'] = arquivo.id;
+        window.location = "/createPlace.html"
+    });
+}
