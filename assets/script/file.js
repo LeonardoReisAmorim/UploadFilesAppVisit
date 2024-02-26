c66e3b947bae75d41c7073fe8dcbc6403f34d1f6
Leonardo Amorim <leonardo.amorim@gomara.tech>
2024-02-26 16:23:34 -0300

    inclusao de modal de edicao e update

---
+++ assets/script/file.js
@@ -0,0 +1,171 @@
+var url = "https://localhost:7011/";
+
+fetch(url+"Arquivo/dadosArquivos")
+.then(response => {
+    if(!response.ok){
+        alert('falhou a requisição');
+        return;
+    }
+
+    if (response.status === 404) {
+        alert('não encontrou qualquer resultado');
+        return;
+    }
+
+    document.getElementById("loading").style.display = "none";
+
+    response.json()
+    .then(files => {
+        files.forEach(element => {
+            var table = document.getElementById("tableFile");
+            var row = document.createElement("tr");
+            row.innerHTML = 
+            `<tr>
+               <td>${element.id}</td>
+               <td>${element.nomeArquivo}</td>
+               <td>${element.dataCriacao}</td>
+               <td>
+               <div style="display: flex;flex-wrap: nowrap; justify-content: center">
+                    <div style="margin-right:20px"><button type="button" onclick="editarArquivo(${element.id})">Editar Arquivo</button></div>
+                    <div><button type="button" onclick="apagarArquivo(${element.id})">Apagar Arquivo</button></div>
+                </div>
+               </td>
+           </tr> `
+           table.appendChild(row);
+        });
+    });
+});
+
+function editarArquivo(id){
+    criarModal();
+
+    fetch(url+`Arquivo/dadosArquivos/${id}`)
+    .then(response => {
+        if(!response.ok){
+            alert('falhou a requisição');
+            return;
+        }
+
+        if (response.status === 404) {
+            alert('não encontrou qualquer resultado');
+            return;
+        }
+
+        response.json()
+        .then(file => {
+            document.getElementById("nomeArquivo").value = file[0].nomeArquivo;
+            document.getElementById("labelEditFile").style.display = "block";
+            document.getElementById("idFile").value = file[0].id;
+        });
+    });
+}
+
+function apagarArquivo(id){
+    fetch(url+`Arquivo/${id}`,{
+        method: 'DELETE'
+    })
+    .then(response => {
+        if(!response.ok){
+            alert('falhou a requisição');
+            return;
+        }
+    
+        if (response.status === 404) {
+            alert('não encontrou qualquer resultado');
+            return;
+        }
+
+        alert('arquivo apagado com sucesso');
+        location.reload();
+    });
+}
+
+function InputFileChange() {
+    let quantidade = document.getElementById("selecao-arquivo").files.length;
+    if (quantidade > 0) {
+        document.getElementById('salvar-anexos').style.display = "block";
+    }
+    else {
+        document.getElementById('salvar-anexos').style.display = "none";
+    }
+    document.getElementById("labelArquivo").innerHTML = `${quantidade} Arquivo(s) Selecionado(s)`; 
+}
+
+function ChamarAjaxComArquivos() {
+    let formData = new FormData();
+    let file = document.getElementById("selecao-arquivo").files[0];
+    let nomearquivo = document.getElementById("nomeArquivo").value;
+    let idFile = document.getElementById("idFile").value
+    let urlFile;
+    let method;
+
+    if(!idFile){
+        urlFile = url+"Arquivo";
+        method = "POST";
+    }else{
+        urlFile = url+`Arquivo/${idFile}`
+        method = "PUT"
+    }
+
+    if(!nomearquivo){
+        alert("necessario do nome do arquivo");
+        return
+    }
+
+    formData.append("selecao-arquivo", file);
+
+    formData.append("dadosArquivo", JSON.stringify({
+        NomeArquivo: nomearquivo,
+        DataCriacao: new Date().toLocaleString('en-US').replace(',','')
+    }));
+
+    try{
+        fetch(urlFile, {
+            method: method,
+            body: formData
+        })
+        .then((result) => {
+            if(!result.ok){
+                alert(result.status);
+                return
+            }
+
+            if(result.status === 204){
+                alert("Arquivo atualizado com sucesso");
+                window.location = "/assets/pages/file/File.html";
+            }
+
+            result.json().then(arquivo => {
+                alert("Arquivo importado com sucesso");
+                window.location = "/assets/pages/file/File.html";
+            });
+        })
+        
+    }catch(error){
+        alert(error);
+    }
+}
+
+function criarModal(){
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
+        refreshDados();
+      modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            refreshDados();
+            modal.style.display = "none";
+        }
+    }
+}
+
+
+function refreshDados(){
+    document.getElementById("nomeArquivo").value = "";
+    document.getElementById("labelEditFile").style.display = "none";
+    document.getElementById("idFile").value = "";
+}
---
+++ assets/script/place.js
@@ -0,0 +1,246 @@
+var url = "https://localhost:7011/";
+
+fetch(url+"Lugar")
+.then(response => {
+    if(!response.ok){
+        alert('falhou a requisição');
+        return;
+    }
+
+    if (response.status === 404) {
+        alert('não encontrou qualquer resultado');
+        return;
+    }
+
+    document.getElementById("loading").style.display = "none";
+
+    response.json().then(places => {
+        places.forEach(element => {
+            var table = document.getElementById("tablePlace");
+            var row = document.createElement("tr");
+            row.innerHTML = 
+            `<tr>
+               <td>${element.id}</td>
+               <td>${element.nome}</td>
+               <td>${element.descricao}</td>
+               <td>${element.cidade}</td>
+               <td>${element.nomeArquivo}</td>
+               <td>
+               <div style="display: flex;flex-wrap: nowrap; justify-content: center">
+                    <div style="margin-right:20px"><button type="button" onclick="editarLugar(${element.id})">Editar Lugar</button></div>
+                    <div><button type="button" onclick="apagarLugar(${element.id})">Apagar Lugar</button></div>
+                </div>
+               </td>
+           </tr> `
+           table.appendChild(row);
+        });
+    });
+});
+
+function editarLugar(id){
+    criarModal();
+
+    fetch(url+`Lugar/${id}`)
+    .then(response => {
+        if(!response.ok){
+            alert('falhou a requisição');
+            return;
+        }
+
+        if (response.status === 404) {
+            alert('não encontrou qualquer resultado');
+            return;
+        }
+
+        response.json()
+        .then(place => {
+            document.getElementById("idplace").value = place[0].id;
+            document.getElementById("namePlace").value = place[0].nome;
+            document.getElementById("descriptionPlace").value = place[0].descricao;
+            document.getElementById("cities").value = place[0].cidadeId;
+            document.getElementById("files").value = place[0].arquivoId;
+            document.getElementById("display").src = "data:image/png;base64,"+place[0].imagem;
+            document.getElementById("display").style.width = "100%";
+            document.getElementById("display").style.height = "500px";
+            document.getElementById("createPlace").innerHTML = "EDITAR LUGAR"
+        });
+    });
+}
+
+function apagarLugar(id){
+    fetch(url+`Lugar/${id}`,{
+        method: 'DELETE'
+    })
+    .then(response => {
+        if(!response.ok){
+            alert('falhou a requisição');
+            return;
+        }
+    
+        if (response.status === 404) {
+            alert('não encontrou qualquer resultado');
+            return;
+        }
+
+        alert('lugar apagado com sucesso');
+        location.reload();
+    });
+}
+
+fetch(url+"Cidade")
+.then(response => {
+    if(!response.ok){
+        alert('falhou a requisição')
+        return 
+    }
+
+    if (response.status === 404) {
+        alert('não encontrou qualquer resultado')
+        return
+    }
+
+    response.json().then(cidades => {
+        for(var cidade of cidades){
+            let option = document.createElement("option");
+            option.value = cidade.id;
+            option.text = cidade.nome;
+            document.getElementById("cities").appendChild(option)
+        }
+    });
+});
+
+fetch(url+"Arquivo/dadosArquivos")
+.then(response => {
+    if(!response.ok){
+        alert('falhou a requisição')
+        return 
+    }
+
+    if (response.status === 404) {
+        alert('não encontrou qualquer resultado')
+        return
+    }
+
+    response.json().then(files => {
+        for(var file of files){
+            let option = document.createElement("option");
+            option.value = file.id;
+            option.text = file.nomeArquivo;
+            document.getElementById("files").appendChild(option)
+        }
+    });
+});
+
+document.getElementById("createPlace").addEventListener('click', ()=>{
+    let cidadeId = parseInt(document.getElementById("cities").value);
+    let nomeLugar = document.getElementById("namePlace").value;
+    let descricaoLugar = document.getElementById("descriptionPlace").value;
+    let imagemBase64 = document.getElementById("display").src.replace('data:', '').replace(/^.+,/, '');
+    let arquivoId = parseInt(document.getElementById("files").value);
+    let idplace = document.getElementById("idplace").value
+    let urlplace;
+    let method;
+
+    if(!idplace){
+        urlplace = url+"Lugar";
+        method = "POST";
+    }else{
+        urlplace = url+`Lugar/${idplace}`
+        method = "PUT"
+    }
+
+    let lugar = {
+        Nome: nomeLugar,
+        Descricao: descricaoLugar,
+        ArquivoId: arquivoId,
+        CidadeId: cidadeId,
+        Imagem: imagemBase64
+    }
+
+    if(!cidadeId || !nomeLugar || !descricaoLugar || !arquivoId){
+        alert("necessario preencher todos os campos");
+        return
+    }
+
+    CreateOrEditPlace(lugar, urlplace, method);
+});
+
+function CreateOrEditPlace(lugarParametro, urlplace, method){
+    try{
+        fetch(urlplace, {
+            method: method,
+            body: JSON.stringify(lugarParametro),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then(result => {
+            if(!result.ok){
+                alert('falhou a requisição')
+                return 
+            }
+    
+            if (result.status === 404) {
+                alert('não encontrou qualquer resultado')
+                return
+            }
+
+            if(result.status == 204){
+                alert("lugar atualizado com sucesso");
+                window.location = "/assets/pages/place/Place.html";
+                return
+            }
+    
+            alert("lugar criado com sucesso");
+            window.location = "/assets/pages/place/Place.html";
+        });
+    }catch(error){
+        alert(error);
+    }
+    
+}
+
+document.getElementById("imagePlace").addEventListener('change', ()=>{
+    var file = document.getElementById("imagePlace").files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+        var imageDisplay = document.getElementById("display");
+        imageDisplay.src = reader.result;
+        imageDisplay.style.width = "100%";
+        imageDisplay.style.height = "500px";
+    };
+    reader.onerror = function (error) {
+        console.log('Error: ', error);
+    };
+});
+
+function criarModal(){
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
+        refreshDados();
+      modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            refreshDados();
+            modal.style.display = "none";
+        }
+    }
+}
+
+
+function refreshDados(){
+    document.getElementById("idplace").value = "";
+    document.getElementById("namePlace").value = "";
+    document.getElementById("descriptionPlace").value = "";
+    document.getElementById("cities").value = "";
+    document.getElementById("files").value = "";
+    document.getElementById("display").src = "";
+    document.getElementById("display").style.width = "0";
+    document.getElementById("display").style.height = "0";
+    document.getElementById("createPlace").innerHTML = "CRIAR LUGAR"
+}
