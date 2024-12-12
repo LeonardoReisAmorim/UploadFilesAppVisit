f761531f22d2de5037c75599dfb805e2b899d88d
Leonardo Amorim <leonardo.amorim@gomara.tech>
2024-12-12 16:05:51 -0300

    add typePlace

--- assets/script/place.js
+++ assets/script/place.js
@@ -175,6 +175,38 @@ GetCidades().then(cidades => {
     }
 });
 
+async function GetTypePlaces(){
+    let response = await fetch(url+"TypePlace", {
+        headers: {Authorization: `Bearer ${token}`}
+    });
+
+    if(!response.ok){
+        document.getElementById("loading").style.display = "none";
+        response.text().then(x => {
+            const match = x.match(/^System\.Exception: .*/m);
+            const exceptionLine = match ? match[0] : null;
+            alert(exceptionLine.split(": ")[1]);
+        });
+        return
+    }
+    
+    if (response.status === 404) {
+        alert('não encontrou qualquer resultado')
+        return
+    }
+
+    return await response.json()
+}
+
+GetTypePlaces().then(typeplaces => {
+    for(var typeplace of typeplaces){
+        let option = document.createElement("option");
+        option.value = typeplace.id;
+        option.text = typeplace.type;
+        document.getElementById("typePlaces").appendChild(option)
+    }
+});
+
 async function GetDadosArquivos(){
     let response = await fetch(url+"FileVR/dadosArquivos", {
         headers: {Authorization: `Bearer ${token}`}
@@ -207,19 +239,20 @@ GetDadosArquivos().then(files => {
     }
 });
 
-document.getElementById("createPlace").addEventListener('click', ()=>{
+document.getElementById("createPlace").addEventListener('click', async ()=>{
     let cidadeId = parseInt(document.getElementById("cities").value);
     let nomeLugar = document.getElementById("namePlace").value;
     let descricaoLugar = document.getElementById("descriptionPlace").value;
     let imagemBase64 = document.getElementById("display").src.replace('data:', '').replace(/^.+,/, '');
     let arquivoId = parseInt(document.getElementById("files").value);
+    let typePlaceId = parseInt(document.getElementById("typePlaces").value);
     let idplace = document.getElementById("idplace").value
     let instrucoesUtilizacaoVR = document.getElementById("instructionVRPlace").value;
     let urlplace;
     let method;
 
-    if(!cidadeId || !nomeLugar || !descricaoLugar || !arquivoId || !instrucoesUtilizacaoVR){
-        alert("os campos Cidade, Nome, Descricao, Arquivo e Instrucoes são obrigatórias");
+    if(!cidadeId || !nomeLugar || !descricaoLugar || !arquivoId || !instrucoesUtilizacaoVR || !typePlaceId){
+        alert("os campos Cidade, Nome, Descricao, Arquivo, Instrucoes e tipo de lugar são obrigatórias");
         return
     }
 
@@ -237,10 +270,11 @@ document.getElementById("createPlace").addEventListener('click', ()=>{
         FileVRId: arquivoId,
         CityId: cidadeId,
         Image: imagemBase64,
-        UsageInstructionsVR: instrucoesUtilizacaoVR
+        UsageInstructionsVR: instrucoesUtilizacaoVR,
+        TypePlaceId: typePlaceId
     }
 
-    CreateOrEditPlace(lugar, urlplace, method);
+    await CreateOrEditPlace(lugar, urlplace, method);
 });
 
 async function CreateOrEditPlace(lugarParametro, urlplace, method){
@@ -293,6 +327,76 @@ async function CreateOrEditPlace(lugarParametro, urlplace, method){
     
 }
 
+document.getElementById("createTypePlace").addEventListener('click', async ()=>{
+    let nameTypePlace = document.getElementById("nameTypePlace").value;
+    let imagemBase64TypePlace = document.getElementById("displayTypePlace").src.replace('data:', '').replace(/^.+,/, '');
+
+    if(!nameTypePlace && !imagemBase64TypePlace){
+        alert("necessario informar todos os dados");
+        return;
+    }
+
+    let typePlaceDTO = {
+        Type: nameTypePlace,
+        Image: imagemBase64TypePlace
+    }
+
+    document.getElementById("loading").style.display = "block";
+    document.getElementById("loading").style.zIndex = 99999;
+
+    try{
+        let response = await fetch(url+"TypePlace", {
+            method: "POST",
+            body: JSON.stringify(typePlaceDTO),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+                "Authorization": "Bearer " + token
+            }
+        })
+
+        if(!response.ok){
+            document.getElementById("loading").style.display = "none";
+            response.text().then(x => {
+                const match = x.match(/^System\.Exception: .*/m);
+                const exceptionLine = match ? match[0] : null;
+                alert(exceptionLine.split(": ")[1]);
+            });
+            return
+        }
+
+        if (response.status === 404) {
+            alert('não encontrou qualquer resultado')
+            return
+        }
+
+        document.getElementById("loading").style.display = "none";
+        alert("tipo de lugar criado com sucesso");
+        await GetTypePlaces();
+        let selectTypePlace = document.getElementById("typePlaces");
+        for (let i = selectTypePlace.options.length - 1; i >= 0; i--) {
+            let item = selectTypePlace.options[i];
+            if (!item.text.includes("Escolha uma")) {
+                selectTypePlace.removeChild(item);
+            }
+        }
+        GetTypePlaces().then(typeplaces => {
+            for(var typeplace of typeplaces){
+                let option = document.createElement("option");
+                option.value = typeplace.id;
+                option.text = typeplace.type;
+                document.getElementById("typePlaces").appendChild(option)
+            }
+        });
+        fecharModalTypePlace();
+
+    }catch(error){
+        fecharModal();
+        document.getElementById("loading").style.display = "none";
+        document.getElementsByClassName("containerTable")[0].style.display = "none";
+        document.getElementsByClassName("errorServer")[0].style.display = "block";
+    }
+});
+
 document.getElementById("imagePlace").addEventListener('change', ()=>{
     var file = document.getElementById("imagePlace").files[0];
     var reader = new FileReader();
@@ -307,6 +411,38 @@ document.getElementById("imagePlace").addEventListener('change', ()=>{
         console.log('Error: ', error);
     };
 });
+
+document.getElementById("imageTypePlace").addEventListener('change', ()=>{
+    var file = document.getElementById("imageTypePlace").files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+        var imageDisplay = document.getElementById("displayTypePlace");
+        imageDisplay.src = reader.result;
+        imageDisplay.style.width = "30%";
+        imageDisplay.style.height = "30%";
+    };
+    reader.onerror = function (error) {
+        console.log('Error: ', error);
+    };
+});
+
+document.getElementById("showModalTypePlace").addEventListener('click', ()=>{
+    var modal = document.getElementById("myModalTypePlace");
+    modal.style.display = "block";
+
+    var span = document.getElementsByClassName("close1")[1];
+    span.onclick = function() {
+        refreshDados();
+      modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            refreshDados();
+            modal.style.display = "none";
+        }
+    }
+})
 
 function criarModal(){
     var modal = document.getElementById("myModal");
@@ -330,11 +466,17 @@ function fecharModal(){
     modal.style.display = "none";
 }
 
+function fecharModalTypePlace(){
+    var modal = document.getElementById("myModalTypePlace");
+    modal.style.display = "none";
+}
+
 function refreshDados(){
     document.getElementById("idplace").value = "";
     document.getElementById("namePlace").value = "";
     document.getElementById("descriptionPlace").value = "";
     document.getElementById("cities").value = "";
+    document.getElementById("typePlaces").value = "";
     document.getElementById("files").value = "";
     document.getElementById("instructionVRPlace").value = "";
     document.getElementById("display").src = "";
